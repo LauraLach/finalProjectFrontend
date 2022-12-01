21ed861a465ce9a900f8bbd5e87f935d5029e0de
Laura Lachin <lauralachin@10-18-185-25.dynapool.wireless.nyu.edu>
2022-12-01 09:44:49 -0500

    Added pages and adjusted styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import './App.css';
+import CreateUserPage from "./pages/CreateUser";
+import LoginPage from "./pages/Login";
+import UserProfilePage from "./pages/UserProfile";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDw5rGNXlyQint4kTYVpiTVmyJzhNwOTRk",
+  authDomain: "final-b343f.firebaseapp.com",
+  projectId: "final-b343f",
+  storageBucket: "final-b343f.appspot.com",
+  messagingSenderId: "511662974154",
+  appId: "1:511662974154:web:e96304af0b63538334c033",
+  measurementId: "G-0F879D8B8F"
+};
 
 function App() {
+  const [appInitialized, setAppInitialized] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInformation, setUserInformation] = useState({});
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if(appInitialized) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if(user) {
+          setUserInformation(user);
+          setIsLoggedIn(true);
+        } else {
+          setUserInformation({});
+          setIsLoggedIn(false);
+        }
+        setIsLoading(false);
+      });
+    }
+  }, [appInitialized]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <UserProfilePage 
+          isLoading={isLoading} 
+          isLoggedIn={isLoggedIn}
+          userInformation={userInformation}
+          setIsLoggedIn={setIsLoggedIn}
+          setUserInformation={setUserInformation}
+        />
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <LoginPage 
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          setUserInformation={setUserInformation}
+        />
+      ),
+    },
+    {
+      path: "/create",
+      element: (
+        <CreateUserPage
+        isLoggedIn={isLoggedIn} 
+        setIsLoggedIn={setIsLoggedIn} 
+        setUserInformation={setUserInformation}
+        />
+      ),
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
